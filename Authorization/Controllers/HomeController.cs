d597209e5022aeac5504b74c009391ff7c555259
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-04-16 00:41:19 +0430

    Add project files.

---
+++ Authorization/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Controllers
+{
+
+
+
+
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ Authorization/Models/AppUser.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Authorization.Models
+{
+
+
+
+
+
+    public class AppUser:IdentityUser
+    {
+    }
+}
