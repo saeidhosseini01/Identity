7bc2d412d8e57db8d8d81adb5d1771261893c07e
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-04-21 00:28:28 +0430

    claimsController

---
+++ Authorization/Controllers/ClaimsController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Controllers
+{
+    public class ClaimsController : Controller
+    {
+
+        [Authorize]
+        public IActionResult Index()
+        {
+            return View(User?.Claims);
+        }
+    }
+}
--- Authorization/Controllers/RoleManagerController.cs
+++ Authorization/Controllers/RoleManagerController.cs
@@ -16,5 +16,18 @@ namespace Authorization.Controllers
             var roles=role.Roles.ToList();
             return View(roles);
         }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult Create(string name) {
+            if (!role.RoleExistsAsync(name).Result)
+            {
+
+          var res=  role.CreateAsync(new IdentityRole (name)).Result;
+            }
+            return RedirectToAction("Index");
+        }
     }
 }
