cdd25d29b1a8f7a3c75ebd5b9fccd5c239e02306
SeyedsaeidHosseini <SeyedsaeidHosseini1990@gmail.com>
2025-04-20 23:42:41 +0430

    ShowListRole

---
+++ Authorization/Controllers/RoleManagerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Authorization.Controllers
+{
+    public class RoleManagerController : Controller
+    {
+        private readonly RoleManager<IdentityRole> role;
+
+        public RoleManagerController(RoleManager<IdentityRole> role)
+        {
+            this.role = role;
+        }
+        public IActionResult Index()
+        {
+            var roles=role.Roles.ToList();
+            return View(roles);
+        }
+    }
+}
